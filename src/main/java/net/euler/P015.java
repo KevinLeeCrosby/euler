d38dbdbc37d58646784184fb6a7d42beaf2a8024
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2014-11-20 10:41:27 -0600

    Removed unused factorial method from Problem 15.

--- src/main/java/net/euler/P015.java
+++ src/main/java/net/euler/P015.java
@@ -7,10 +7,6 @@ import org.apache.commons.math3.util.CombinatoricsUtils;
 public class P015 {
   private static final int n = 20; // lattice size  (NOTE:  over 14 gives overflow errors
 
-  private static long factorial(int n) {
-    return n < 2 ? 1 : n * factorial(n - 1);
-  }
-
   public static void main(String[] args) {
     long noRoutes = CombinatoricsUtils.binomialCoefficient(2 * n, n);
 
