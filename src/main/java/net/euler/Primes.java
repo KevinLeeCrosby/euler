b81d9b7d3345e3cdf9a2ad1f7566ce70913705b0
Kevin Crosby <Kevin.L.Crosby@gmail.com>
2014-12-31 00:13:29 -0600

    Added dependency to pom.xml.

--- src/main/java/net/euler/Primes.java
+++ src/main/java/net/euler/Primes.java
@@ -2,14 +2,14 @@ package net.euler;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import org.apache.commons.math3.util.Pair;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import static net.euler.MathUtils.*;
+import static net.euler.MathUtils.gcd;
+import static net.euler.MathUtils.pow;
 
 /**
  * Prime number generator (64 bit) and related methods.
