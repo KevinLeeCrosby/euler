febe34c00e3c1a0b8a4a90f9203a9c5587452b30
Kevin Crosby <Kevin.L.Crosby@gmail.com>
2015-01-01 16:19:54 -0600

    Changed JavaDoc on Problem 56.

--- src/main/java/net/euler/P056.java
+++ src/main/java/net/euler/P056.java
@@ -6,7 +6,8 @@ import static java.math.BigInteger.*;
 import static java.math.BigInteger.valueOf;
 
 /**
- * A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+ * A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large:
+ * one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
  * <p/>
  * Considering natural numbers of the form, a^b, where a, b < 100, what is the maximum digital sum?
  *
