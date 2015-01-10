2d450c7889cb0a2cd6d696a71b2a896aa822e76a
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2015-01-09 19:34:38 -0600

    Added JavaDoc and tweaked Problem 26.

--- src/main/java/net/euler/P026.java
+++ src/main/java/net/euler/P026.java
@@ -6,7 +6,24 @@ import java.math.BigInteger;
 import java.util.Set;
 
 /**
- * Created by Kevin on 11/28/2014.
+ * A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to
+ * 10 are given:
+ *
+ * 1/2  = 0.5
+ * 1/3  = 0.(3)
+ * 1/4  = 0.25
+ * 1/5  = 0.2
+ * 1/6  = 0.1(6)
+ * 1/7  = 0.(142857)
+ * 1/8  = 0.125
+ * 1/9  = 0.(1)
+ * 1/10 = 0.1
+ * Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring
+ * cycle.
+ *
+ * Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+ *
+ * @author Kevin Crosby
  */
 public class P026 {
   private static final Primes P = Primes.getInstance();
@@ -55,12 +72,7 @@ public class P026 {
   }
 
   public static void main(String[] args) {
-    long limit;
-    if (args.length > 0) {
-      limit = Long.parseLong(args[0]);
-    } else {
-      limit = 1000;
-    }
+    final long limit = args.length > 0 ? Long.parseLong(args[0]) : 1000L;
 
     long period = 0;
     long d = 1;
