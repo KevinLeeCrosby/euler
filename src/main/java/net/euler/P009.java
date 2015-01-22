d34f22cee77c63aab00c02955e91f2c4d04a6849
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2015-01-22 12:48:34 -0600

    Added JavaDoc to Problem 9.

--- src/main/java/net/euler/P009.java
+++ src/main/java/net/euler/P009.java
@@ -1,11 +1,19 @@
 package net.euler;
 
 /**
- * Created by kevin on 11/12/14.
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ * a² + b² = c²
+ * For example, 3² + 4² = 9 + 16 = 25 = 5².
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ *
+ * @author Kevin Crosby
  */
 public class P009 {
   public static void main(String[] args) {
-    int sum = 1000;
+    final int sum = args.length > 0 ? Integer.parseInt(args[0]) : 1000;
 
     for (int a = 1; a < sum * 0.3; a++) { // upper bound of a is sum * (2 - sqrt(2)) / 2
       for (int b = (int)Math.ceil(sum * 0.3f); b < sum * 0.42; b++) { // lower bound of b is sum * (2 - sqrt(2)) / 2 , upper bound of b is sum * (sqrt(2) - 1)
