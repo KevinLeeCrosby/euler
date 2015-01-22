bd026ae36f874adff24db597229711fc1b2d4637
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2015-01-22 13:56:18 -0600

    Fixed Problem 9 JavaDoc.

--- src/main/java/net/euler/P009.java
+++ src/main/java/net/euler/P009.java
@@ -1,9 +1,8 @@
 package net.euler;
 
 /**
- * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a² + b² = c²
  *
- * a² + b² = c²
  * For example, 3² + 4² = 9 + 16 = 25 = 5².
  *
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
