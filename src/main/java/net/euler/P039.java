72a9690c7615a2747593d25aa43e2e09de50220c
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2015-01-22 12:34:19 -0600

    Fixed JavaDoc for Problem 39.

--- src/main/java/net/euler/P039.java
+++ src/main/java/net/euler/P039.java
@@ -10,10 +10,11 @@ import java.util.Set;
 import static net.euler.MathUtils.gcd;
 
 /**
- * If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
- * <p/>
+ * If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three
+ * solutions for p = 120.
+ *
  * {20,48,52}, {24,45,51}, {30,40,50}
- * <p/>
+ *
  * For which value of p ≤ 1000, is the number of solutions maximised?
  *
  * @author Kevin Crosby
