28396aa1adc513dc6cf43dbe2027cc0decffe00a
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2015-01-22 19:35:14 -0600

    Finished Problem 75.

--- src/main/java/net/euler/P075.java
+++ src/main/java/net/euler/P075.java
@@ -1,16 +1,16 @@
 package net.euler;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.tuple.Triple;
 
-import java.util.Set;
+import java.util.Map;
 
 import static net.euler.MathUtils.gcd;
+import static net.euler.MathUtils.sqrt;
 
 /**
- * It turns out that 12 cm is the smallest length of wire that can be bent to form an integer sided right angle triangle
+ * It turns out that 12 cm is the smallest length of wire that can be bent to form an integer sided right angle
+ * triangle
  * in exactly one way, but there are many more examples.
  *
  * 12 cm: (3,4,5)
@@ -32,51 +32,42 @@ import static net.euler.MathUtils.gcd;
  * @author Kevin Crosby
  */
 public class P075 {
-  private static Primes primes = Primes.getInstance();
-
   // generalized Euclid's formula
-  private static Triple<Integer, Integer, Integer> triple(int k, int m, int n) {
-    return Triple.of(k * (m * m - n * n), 2 * k * m * n, k * (m * m + n * n));
-  }
+  //private static Triple<Integer, Integer, Integer> triple(final int k, final int m, final int n) {
+  //  return Triple.of(k * (m * m - n * n), 2 * k * m * n, k * (m * m + n * n));
+  //}
 
-  private static Integer perimeter(int k, int m, int n) {
+  private static Integer perimeter(final int k, final int m, final int n) {
     return 2 * k * m * (m + n);
   }
 
   public static void main(String[] args) {
-    int maxPerimeter;
-    if (args.length > 0) {
-      maxPerimeter = Integer.parseInt(args[0]);
-    } else {
-      maxPerimeter = 1000;
-    }
+    final int maxPerimeter = args.length > 0 ? Integer.parseInt(args[0]) : 1500000;
 
     // generate all Pythagorean triples with perimeter up to max perimeter
-    Multimap<Integer, Triple<Integer, Integer, Integer>> map = ArrayListMultimap.create();
+    Map<Integer, Integer> map = Maps.newHashMap();
     int maxSemiPerimeter = maxPerimeter / 2;
-    for (int k = 1; k <= maxSemiPerimeter / 2; k++) { // k = 1 produces primitive Pythagorean triples (k = gcd(a, c))
-      for (int m = 2; m < maxSemiPerimeter / k; m++) {
-        for (int n = 1 + m % 2; n < m; n += 2) { // force m - n to be odd
-          int p = perimeter(k, m, n);
-          if (gcd(m, n) != 1 || p > maxPerimeter) {
-            continue;
+    for (int m = 2; m < sqrt(maxSemiPerimeter); m++) {
+      for (int n = 1 + m % 2; n < m; n += 2) { // force m - n to be odd
+        if (gcd(m, n) == 1) {
+          int p = perimeter(1, m, n); // k = 1 produces primitive Pythagorean triples (k = gcd(a, c))
+          for (int k = 1, perimeter = p; perimeter <= maxPerimeter; k++, perimeter += p) {
+            //System.out.println(perimeter + ": " + triple(k, m, n));
+            int count = map.containsKey(perimeter) ? map.get(perimeter) : 0;
+            map.put(perimeter, ++count);
           }
-          map.put(p, triple(k, m, n));
         }
       }
     }
 
-    int maxSolutions = 0;
-    int bestPerimeter = 0;
-    for (int p : Sets.newTreeSet(map.keySet())) {
-      Set<Triple<Integer, Integer, Integer>> triples = Sets.newTreeSet(map.get(p));
-      if (triples.size() > maxSolutions) {
-        maxSolutions = triples.size();
-        bestPerimeter = p;
+    int total = 0;
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if (entry.getValue() == 1) {
+        total++;
       }
-      System.out.println(p + ": " + Sets.newTreeSet(map.get(p)));
     }
-    System.out.println();
-    System.out.println("The best value of p ≤ " + maxPerimeter + ", is " + bestPerimeter + ", where the number of solutions maximised (" + maxSolutions + ").");
+
+    System.out.println("There are " + total + " values of L ≤ " + maxPerimeter
+        + " where exactly one integer sided right angle triangle can be formed.");
   }
 }
